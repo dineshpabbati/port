3770b2824841ce176af9b254509b24d48ff805ac
sriramvoruganti <sriramaries28@gmail.com>
2024-01-26 14:25:21 -0500

    changes to Readme

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,7 +105,7 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
+        <a href="https://github.com/Sriram-Northeastern">
           <div>Designed &amp; Built by Brittany Chiang, Revised by Sriram Voruganti</div>
 
           {githubInfo.stars && githubInfo.forks && (
