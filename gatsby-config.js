7b1bae7d48492fc3395f9caa7fbf9cb2986a5641
dineshpabbati <113916838+dineshpabbati@users.noreply.github.com>
2024-06-28 21:30:11 -0600

    portfolio updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Sriram Voruganti',
+    title: 'Dinesh Pabbati',
     description:
-      'Sriram Voruganti is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Dinesh Pabbati is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@kafka5433',
+    twitterUsername: '@dineshpabbati',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Sriram Voruganti',
-        short_name: 'Sriram Voruganti',
+        name: 'dineshpabbati',
+        short_name: 'dineshpabbati',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
