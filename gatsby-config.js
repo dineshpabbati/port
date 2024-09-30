b273ca0d69b023608948eadb80b5cd9c5cd6c5eb
dineshpabbati <113916838+dineshpabbati@users.noreply.github.com>
2024-09-29 23:52:21 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Dinesh Pabbati',
     description:
       'Dinesh Pabbati is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://dineshpabbati.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@dineshpabbati',
   },
